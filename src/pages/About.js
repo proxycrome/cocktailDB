6eb7bdf8fa7da7b884908e58a45c916dc1f4d42a
proxycrome <78900687+proxycrome@users.noreply.github.com>
2022-01-16 20:37:13 +0100

    app ready

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste,
+        voluptatem! In odit, porro sequi quod iste dolore facilis nisi cum, ad
+        consequuntur molestiae cupiditate voluptates asperiores sapiente iusto
+        non rerum dolores perferendis eos accusantium, aliquid nesciunt nobis?
+        Quo, mollitia magnam!
+      </p>
+    </section>
+  );
+};
+
+export default About;
